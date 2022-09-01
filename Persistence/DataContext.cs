72e28fca1d91b769d3d12c648d67d004e7b04685
HungDo007 <beekunfar@gmail.com>
2022-09-01 10:51:42 +0700

    CRUD activity API using cqrs and mediator

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,6 +12,6 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<Activity> Activities => Set<Activity>();
+        public DbSet<Activity> Activities { get; set; }
     }
 }
