9a8e539c7eabb2b528976951ed6e0a59a329c819
HungDo007 <beekunfar@gmail.com>
2022-09-20 18:17:56 +0700

    Add error handling

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Router } from "react-router-dom";
 import { store, StoreContext } from "./app/stores/store";
 import "react-calendar/dist/Calendar.css";
+import "react-toastify/dist/ReactToastify.min.css";
 
 import App from "./app/layout/App";
 import "./app/layout/styles.css";
+
+import { createBrowserHistory } from "history";
+
+//Router need History<unknow>
+export const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <StoreContext.Provider value={store}>
-    <BrowserRouter>
+    <Router history={history}>
       <App />
-    </BrowserRouter>
+    </Router>
   </StoreContext.Provider>
 );
 
